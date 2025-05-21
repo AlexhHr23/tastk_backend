1d6af9c4bdd218a2634c60b406b3281533496bc6
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-05-20 21:46:30 -0600

    Creando routing para proyecos

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,12 @@
+import type { Request, Response } from "express"
+
+
+export class ProjectController {
+
+    static getAllProjects = async (req: Request, res: Response) => {
+
+        res.send('Todos los proyects')
+
+    }
+
+}
