50d5201a5696880e8eacfb56c0c76f6cbd1da561
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-06-01 12:20:45 -0600

    Agregabdi validación en el servidro de los proyectos

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
+import { body } from 'express-validator'
 import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middleware/validation";
 
 
 const router  = Router()
 
-router.post('/', ProjectController.createProjects)
+router.post('/', 
+    body('projectName')
+        .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description')
+        .notEmpty().withMessage('Las descripción del proyecto es obligatoria'),
+    
+    handleInputErrors,
+    ProjectController.createProjects
+)
 
 router.get('/', ProjectController.getAllProjects)
 
