fc9f6ca7ab0ccb829a9a736745fb873388aa98bf
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-06-01 15:41:23 -0600

    Creando tareas y asignando proyecto

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,12 +1,26 @@
 import type { Request, Response } from "express";
+import Project from "../models/Project";
+import Task from "../models/Task";
 
 
 export class TaskController {
     static createTask = async(req: Request, res: Response) => {
+        const {projectId} = req.params
+            const project = await Project.findById(projectId)
+
+            if (!project) {
+                const error = new Error('Proyecto no encontrado')
+                res.status(404).json({ error: error.message })
+                return
+            }
         try {
-            
-            const {projectId} = req.params
-            console.log(projectId);
+            const task = new Task(req.body)
+            task.project = project.id
+            project.tasks.push(task.id)
+            await task.save()
+            await project.save()
+            res.send('Tarea creada correctamente')
+            console.log(task);
         } catch (error) {
             console.log(error);
         }
