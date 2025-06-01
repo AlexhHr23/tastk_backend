01905a2a3df3ae03ab00c7b6e77e126ed3d6d18d
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-06-01 14:54:25 -0600

    Relacionado tareas y proyectos

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,9 +1,10 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, Types } from "mongoose";
 
 
 export interface ITask extends Document {
     name: string
     description: string
+    project: Types.ObjectId
 }
 
 
@@ -17,8 +18,13 @@ export const TaskSchema : Schema = new Schema ({
         type: String,
         trim: true,
         required: true
+    },
+    project: {
+        type: Types.ObjectId,
+        ref: 'Project'
     }
-})
+
+}, {timestamps: true})
 
 const Task = mongoose.model<ITask>('Task', TaskSchema)
 
