99efba748fb633d2985956587f8192497a1df6ae
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-06-16 17:58:51 -0600

    Generar token de 6 digitos

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,6 +2,8 @@ import type {Request, Response} from 'express'
 import bcrypt from "bcrypt"
 import User from '../models/User'
 import { hashPassword } from '../utils/auth'
+import Token from '../models/Token'
+import { generateToken } from '../utils/token'
 
 export class AuthController {
     
@@ -20,8 +22,15 @@ export class AuthController {
 
             //Crear usuario
             const user = new User(req.body)
+            //Hashear password
             user.password = await hashPassword(password)
-            await user.save()
+
+            //Generar token
+            const token = new Token()
+            token.token = generateToken()
+            token.user = user.id
+            
+            await Promise.allSettled([user.save(), token.save()])
             res.send('Cuenta creada. revisa tu email para confirmarla')
         } catch (error) {
             console.log(error);
