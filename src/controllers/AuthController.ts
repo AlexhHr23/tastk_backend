33239097dab07556b77fe542a42b70aac648fb80
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-06-16 17:33:07 -0600

    Prevenir usuarios duplicados

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -8,10 +8,18 @@ export class AuthController {
 
     static createAccount = async(req : Request, res: Response) => {
         try {
-            const { password } = req.body
-            const user = new User(req.body)
+            const { password, email } = req.body
 
-            //Hash password
+            //Prevenir duplicados
+            const userExists = await User.findOne({email})
+
+            if(userExists) {
+                const error = new Error('Ya existe un usuario con ese email')
+                res.status(409).json({error: error.message})
+            }
+
+            //Crear usuario
+            const user = new User(req.body)
             user.password = await hashPassword(password)
             await user.save()
             res.send('Cuenta creada. revisa tu email para confirmarla')
